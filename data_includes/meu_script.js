34f71851e0d24886a1dd484be4559a01a7016caa
Vitória Maria Albuquerque Silva <153312271+vitoriamariia@users.noreply.github.com>
2024-06-16 19:46:32 -0300

    Update meu_script.js

--- data_includes/meu_script.js
+++ data_includes/meu_script.js
@@ -16,7 +16,6 @@ Header(
     .wait()
 );
 
-// Tela de identificação do participante
 newTrial("Participantes",
   newText("<p>Olá! Seja bem-vindo ao nosso experimento!</p>")
     .print(),
@@ -35,9 +34,8 @@ newTrial("Participantes",
     .setVar("Curso", getDropDown("Curso").selected())  // Armazena o curso na variável global
 );
 
-// Tela de instruções
 newTrial("Instrucoes",
-  newText("<p>Leia com atenção:</p>")
+  newText("<p>Leia com atencão:</p>")
     .print(),
   newText("<p>Leia a situação fictícia e leia em voz alta a frase em destaque</p>")
     .print(),
@@ -46,9 +44,8 @@ newTrial("Instrucoes",
     .wait()
 );
 
-// Tela de experimento
 newTrial("Experimento",
-  newText("Imagine a seguinte situação: você encontra uma receita de macarrão gourmet no TikTok e resolve recriar essa receita. Suponha que você seguiu a receita à risca, com todos os seus passos. Ao final, prova e gosta muito. Você, então, diz:")
+  newText("Imagine a seguinte situacao: você encontra uma receita de macarrão gourmet no TikTok e resolve recriar essa receita. Suponha que você seguiu a receita à risca, com todos os seus passos. Ao final, prova e gosta muito. Você, então, diz:")
     .print(),
   newText("Ficou muito gostoso, o macarrão")
     .css("font-weight", "bold")
@@ -57,7 +54,7 @@ newTrial("Experimento",
     .print()
     .wait()
 )
-.log("Curso", getVar("Curso"));  // Log do curso selecionado
+.log("Curso", getVar("Curso"));  
 
 // Tela de agradecimento
 newTrial("Final",
