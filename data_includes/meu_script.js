6b173f2d36c915d0f14ae695572f40bdc4022001
Vitória Maria Albuquerque Silva <153312271+vitoriamariia@users.noreply.github.com>
2024-06-16 19:38:45 -0300

    Update meu_script.js

--- data_includes/meu_script.js
+++ data_includes/meu_script.js
@@ -1,6 +1,6 @@
 PennController.ResetPrefix(null);
 
-Sequence("Participantes", "Instrucoes", randomize("Experimento1"), "Final");
+Sequence("Participantes", "Instrucoes", "Experimento", "Final");
 
 Header(
   defaultText
@@ -16,23 +16,28 @@ Header(
     .wait()
 );
 
+// Tela de identificação do participante
 newTrial("Participantes",
-  newText("<p> Olá! Seja bem-vindo ao nosso experimento!</p>")
+  newText("<p>Olá! Seja bem-vindo ao nosso experimento!</p>")
     .print(),
-  newText("<p> Por gentileza, informe o CURSO que faz na UFERSA </p>")
+  newText("<p>Por gentileza, selecione o CURSO que faz na UFERSA</p>")
     .print(),
-  newDropDown("Curso que faz na UFERSA")
-  .add("Engenharia", "Ciencias e Tecnologias", "Letras")
+  newDropDown("Curso")
+    .add("Engenharia", "Ciências e Tecnologias", "Letras")
     .css("font-size", "1.2em")
     .print()
     .log(),
   newButton("Vamos para as instruções")
     .print()
-    .wait(),
+    .wait(
+      getDropDown("Curso").test.selected()  // Certifique-se de que um curso foi selecionado
+    )
+    .setVar("Curso", getDropDown("Curso"))  // Armazena o curso na variável global
 );
 
+// Tela de instruções
 newTrial("Instrucoes",
-  newText("<p>Leia com atencao:</p>")
+  newText("<p>Leia com atenção:</p>")
     .print(),
   newText("<p>Leia a situação fictícia e leia em voz alta a frase em destaque</p>")
     .print(),
@@ -41,16 +46,20 @@ newTrial("Instrucoes",
     .wait()
 );
 
-newTrial("Experimento1",
+// Tela de experimento
+newTrial("Experimento",
   newText("Imagine a seguinte situação: você encontra uma receita de macarrão gourmet no TikTok e resolve recriar essa receita. Suponha que você seguiu a receita à risca, com todos os seus passos. Ao final, prova e gosta muito. Você, então, diz:")
-  .print(),
+    .print(),
   newText("Ficou muito gostoso, o macarrão")
-  .print(),
+    .css("font-weight", "bold")
+    .print(),
   newButton("Próximo")
     .print()
     .wait()
-);
+)
+.log("Curso", getVar("Curso"));
 
+// Tela de agradecimento
 newTrial("Final",
   newText("Obrigada por participar do experimento!")
     .print(),
