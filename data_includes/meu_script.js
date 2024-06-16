8c9d50f36904eef5e1716102b58339420281ce8c
Vitória Maria Albuquerque Silva <153312271+vitoriamariia@users.noreply.github.com>
2024-06-16 19:49:23 -0300

    Update meu_script.js

--- data_includes/meu_script.js
+++ data_includes/meu_script.js
@@ -17,27 +17,29 @@ Header(
 );
 
 newTrial("Participantes",
-  newText("<p>Olá! Seja bem-vindo ao nosso experimento!</p>")
+  newText("<p>Ola! Seja bem-vindo ao nosso experimento!</p>")
     .print(),
   newText("<p>Por gentileza, selecione o CURSO que faz na UFERSA</p>")
     .print(),
   newDropDown("Curso")
-    .add("Engenharia", "Ciências e Tecnologias", "Letras")
+    .add("Engenharia", "Ciencias e Tecnologias", "Letras")
     .css("font-size", "1.2em")
     .print()
     .log(),
-  newButton("Vamos para as instruções")
+  newButton("Vamos para as instrucoes")
     .print()
     .wait(
       getDropDown("Curso").test.selected()  // Certifique-se de que um curso foi selecionado
     )
-    .setVar("Curso", getDropDown("Curso").selected())  // Armazena o curso na variável global
+    .setVar("Curso", getDropDown("Curso").settings.callback(function(dropdown){
+      return dropdown.element.value;
+    })) 
 );
 
 newTrial("Instrucoes",
-  newText("<p>Leia com atencão:</p>")
+  newText("<p>Leia com atencao:</p>")
     .print(),
-  newText("<p>Leia a situação fictícia e leia em voz alta a frase em destaque</p>")
+  newText("<p>Leia a situacao ficticia e leia em voz alta a frase em destaque</p>")
     .print(),
   newButton("Iniciar")
     .print()
@@ -45,18 +47,18 @@ newTrial("Instrucoes",
 );
 
 newTrial("Experimento",
-  newText("Imagine a seguinte situacao: você encontra uma receita de macarrão gourmet no TikTok e resolve recriar essa receita. Suponha que você seguiu a receita à risca, com todos os seus passos. Ao final, prova e gosta muito. Você, então, diz:")
+  newText("Imagine a seguinte situacao: voce encontra uma receita de macarrao gourmet no TikTok e resolve recriar essa receita. Suponha que voce seguiu a receita a risca, com todos os seus passos. Ao final, prova e gosta muito. Voce, entao, diz:")
     .print(),
-  newText("Ficou muito gostoso, o macarrão")
+  newText("Ficou muito gostoso, o macarrao")
     .css("font-weight", "bold")
     .print(),
-  newButton("Próximo")
+  newButton("Proximo")
     .print()
     .wait()
 )
 .log("Curso", getVar("Curso"));  
 
-// Tela de agradecimento
+
 newTrial("Final",
   newText("Obrigada por participar do experimento!")
     .print(),
