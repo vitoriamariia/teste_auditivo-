1586689a908d664a75aa4e81c18d4356d413b06d
Vitória Maria Albuquerque Silva <153312271+vitoriamariia@users.noreply.github.com>
2024-06-16 11:25:01 -0300

    Update meu_script.js

--- data_includes/meu_script.js
+++ data_includes/meu_script.js
@@ -1,92 +1,83 @@
 PennController.ResetPrefix(null);
 
-Sequence("Participantes", "Instrucoes", (randomize("Experimento"),SendResults()"Final");
+Sequence("Participantes", "Instrucoes", randomize("Experimento"), "Final");
 
 Header(
-
-  defaulttext
-    .css("font-size","1.2em")
-    .print()
-  ,
+  defaultText
+    .css("font-size", "1.2em")
+    .print(),
   defaultTextInput
-    .css("font-size","1.2em")
-    .print()
-
-  ,
-
+    .css("font-size", "1.2em")
+    .print(),
   defaultButton
-    .css("font-size","1.2em")
+    .css("font-size", "1.2em")
     .print()
     .center()
     .wait()
+);
 
+newTrial("Participantes",
+  newText("<p> Olá! Seja bem-vindo ao nosso experimento!</p>")
+    .print(),
+  newText("<p> Por gentileza, informe o CURSO que faz na UFERSA </p>")
+    .print(),
+  newDropDown("Curso que faz na UFERSA")
+    .add("Engenharia", "Letras")
+    .css("font-size", "1.2em")
+    .print()
+    .log(),
+  newButton("Iniciar")
+    .print()
+    .wait(),
+  newVar("NOME")
+    .global()
+    .set(getTextInput("Nome"))
 )
+.log("NOME", getVar("NOME"));
 
-newTrial("Participante".
-         
-  newText("<p> Olá! Seja bem-vindo ao nosso experimento!)
-                 
-  newTextInput("Nome")
-                 
-  newText("<p> Por gentileza, informe o seu NOME COMPLETO e o CURSO que faz na UFERSA </p>"
+newTrial("Instrucoes",
+  newText("<p>INSTRUÇÕES:</p>")
+    .print(),
+  newText("<p>Ouça e clique</p>")
+    .print(),
+  newButton("Iniciar")
+    .print()
+    .wait()
+);
 
-  newDropDown("Curso que faz na Ufersa")
-                        .add("Engenharia", "Letras")
-                        .css("font-size", "1.2em")
-                        .print
-                        .log
-,
-          newButton("Iniciar")
-          ,
-          newVar("NOME")
-          .global()
-          .set( getTextInput("Nome"))
-          )
-      .log("NOME", getVar("NOME"))
+Template("tabela_script_auditivo.csv",
+  row => newTrial("Experimento",
+    newAudio("AudioExperimento", row.AudioExperimento)
+      .play(),
+    newImage("alto_falante_Icone.png")
+      .size(90, 90)
+      .print(),
+    newButton("Próximo")
+      .log()
+      .remove(),
+    getImage("alto_falante_Icone.png")
+      .remove(),
+    newText("A", row.SentencaA),
+    newText("B", row.SentencaB),
+    newCanvas("2000vw", "800vh")
+      .add("center at 25%", "middle at 2%", getText("A"))
+      .add("center at 75%", "middle at 2%", getText("B"))
+      .print(),
+    newSelector()
+      .add(getText("A"), getText("B"))
+      .keys("A", "B")
+      .log()
+      .wait()
+  )
+  .log("Group", row.Group)
+  .log("Item", row.Item)
+);
 
-          newTrial("Instrucoes",
-                   newText("<p>INSTRUCOES:</p>")
-                   ,
-                   newText(<p>Ouça e clique</p>")
-                   ,
-                   newButton("Iniciar")
-                   .log()
+newTrial("Final",
+  newText("Obrigada")
+    .print(),
+  newButton("Finalizar")
+    .print()
+    .wait()
 )
-
-          Template("tabela_script_auditivo.csv",
-            row => newTrial("Experimento",
-                            newAudio("AudioExperimento", row.AudioExperimento)
-                            .play
-                            ,
-                            newImage("alto_falante_Icone.png"
-                                     .size(90,90)
-                                     .print
-                                     ,
-                                     newButton("Próximo")
-                                     .log()
-                                     .remove()
-                                     ,
-                                     getImage("alto_falante_Image.png")
-                                     .remove
-                                     ,
-                                     newText("A", row.SentencaA)
-                                     ,
-                                      newText("B", row.SentencaB)
-                                     ,
-                                     newCanvas("2000vw", "800vh")
-                                     .add( "center at 25%", "middle at 2%", getText("A"))
-                                      .add("center at 75% , middle at 2%, getText("B"))
-                                      .print()
-                                     ,
-                            newSelector()
-                            .add(getText("A"), getText("B"))
-                            .keys("A","B")
-                            .log()
-                            .wait()
-          )
-          .log("Group", row.Group) 
-          .log("Item", rowItem)
-                            newTrial("Final",
-                                     newText("Obrigada"
-          )
-          .setOption("countsForProgressBar", false)
+.setOption("countsForProgressBar", false);
