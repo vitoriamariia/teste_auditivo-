f937aeef8e81ab4b0443727bbf8af6397ba848cd
Vitória Maria Albuquerque Silva <153312271+vitoriamariia@users.noreply.github.com>
2024-06-19 13:56:29 -0300

    Update meu_script.js

--- data_includes/meu_script.js
+++ data_includes/meu_script.js
@@ -55,19 +55,46 @@ newTrial("Instrucoes",
     .log()
 );
 
-Template
-newTrial("Experimento",
-  newText("Imagine a seguinte situacao: voce encontra uma receita de macarrao gourmet no TikTok e resolve recriar essa receita. Suponha que voce seguiu a receita a risca, com todos os seus passos. Ao final, prova e gosta muito. Voce, entao, diz:")
-    .print(),
-  newText("Ficou muito gostoso, o macarrao")
-    .css("font-weight", "bold")
-    .print(),
-  newButton("Proximo")
-    .print()
-    .wait()
-    .log()
-);
+Template("teste_auditivo.csv",
+  row =>  newTrial("Experimento",
+    newAudio("AudioExperimento",row,AudioExperimento)
+     .play(),
+                   
+    newImage("alto_falante_icone.png")
+     .size( 90 , 90)
+     .print(),
+    newButton("Próximo")
+      .log()
+      .remove(),
+    getImage(alto_falante_icone.png)
+      .remove(),
+    newText("A", row.SentencaA),
+    newText("B", row.SentencaB),
+    newCanvas("2000vw", "800vw")
+      .add("center at 25%", "middle at 2%", getText("A"))
+      .add("center at 75%", "middle at 2%", getText("B"))
+      .print(),
 
+    newSelector()
+      .add(getText("A"), getText("B"))
+      .keys("A","B")
+      .log
+      .wait
+      )
+    .log("Group",rowGroup)
+    .log("Item". row.Item))
+                  
+  newTrial("Experimento2")
+  newText("Imagine a seguinte situacao: voce encontra uma receita de macarrao gourmet no TikTok e resolve recriar essa receita. Suponha que voce seguiu a receita a risca, com todos os seus passos. Ao final, prova e gosta muito. Voce, entao, diz:")
+     .print(),
+  newText("Ficou muito gostoso, o macarrao")
+     .css("font-weight", "bold")
+     .print(),
+  newButton("Proximo")
+     .print()
+     .wait()
+     .log()
+);
 
 newTrial("Final",
   newText("Obrigada por participar do experimento!")
